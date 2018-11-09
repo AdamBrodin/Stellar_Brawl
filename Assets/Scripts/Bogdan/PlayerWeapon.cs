110477d61c6067d378284124fc6e031e2d70a29b
Adam Brodin <worecraft@gmail.com>
2018-11-09 15:41:03 +0100

    Bug fixes
    A bunch of bug fixes in all areas of the game

--- Assets/Scripts/Bogdan/PlayerWeapon.cs
+++ Assets/Scripts/Bogdan/PlayerWeapon.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour //Bogdan C. SU17A\\
 {
-    public Transform firepointTop;
-    public Transform firepointDown;
+    public Transform firepointTop, firepointDown;
     public GameObject beamPrefab;
-	
-	void Update ()
+    private float cooldown = 0.2f;
+    private bool canShoot = true;
+
+    void Update()
     {
-		if (Input.GetButtonDown("Jump"))
+        if (Input.GetButton("Jump") && canShoot)
         {
+            canShoot = false;
             StartCoroutine(Shoot());
         }
-	}
+    }
 
     // Spawns BeamPrefabs wich as bullets
     private IEnumerator Shoot()
     {
         Instantiate(beamPrefab, firepointTop.position, firepointTop.rotation);
         Instantiate(beamPrefab, firepointDown.position, firepointDown.rotation);
-        yield return new WaitForSeconds(0.5f);
-      //  Debug.Log("SHOOT!");
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
     }
 
 
